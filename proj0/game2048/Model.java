627dedcf488689a9a52cba5d4b0a06566264037a
Lily <lilzzoo0525@outlook.com>
2024-11-16 22:04:41 -0800

    remove todo

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -109,7 +109,6 @@ public class Model extends Observable {
     public boolean tilt(Side side) {
         boolean changed;
         changed = false;
-        // TODO: Modify this.board (and perhaps this.score) to account
         // for the tilt to the Side SIDE. If the board changed, set the
         // changed local variable to true.
         board.setViewingPerspective(side);
